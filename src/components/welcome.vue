9abc980b5ae8b3efe157fef8803ee8c575dabc65
parthobepary <pallobbiswas326@gmail.com>
2024-04-17 19:24:04 +0600

    init

---
+++ src/components/welcome.vue
@@ -0,0 +1,14 @@
+<script setup >
+import SwiperSlide from './slider/SwiperSlide.vue';
+
+
+</script>
+<template>
+    <div class="text-center my-3">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, natus?</p>
+
+        <div>
+            <SwiperSlide />
+        </div>
+    </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container mx-auto text-center my-4">
+        <Welcome />
+    </div>
+</template>
