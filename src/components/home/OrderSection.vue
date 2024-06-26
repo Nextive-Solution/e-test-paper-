acdd109092f9268adbb37d725824b80e96eaa1df
parthobepary <pallobbiswas326@gmail.com>
2024-06-26 23:15:02 +0600

    order done

--- src/components/home/OrderSection.vue
+++ src/components/home/OrderSection.vue
@@ -4,19 +4,22 @@
       <p class="text-[26px] 2xl:text-[46px] font-[700] text-[#0381e0]">প্রয়োজনে কল করুন- 8801646664222</p>
     </div>
     <div class="text-center pt-4">
-      <p class="2xl:text-[26px]  text-[#000000] font-[600]">অর্ডারটি কনফার্ম করতে, আপনার নাম, ঠিকানা ও ফোন নাম্বারটি দিয়ে "অর্ডার কনফার্ম" বাটনে ক্লিক করুন।</p>
+      <p class="2xl:text-[26px]  text-[#000000] font-[600]">অর্ডারটি কনফার্ম করতে, আপনার নাম, ঠিকানা ও ফোন নাম্বারটি
+        দিয়ে "অর্ডার কনফার্ম" বাটনে ক্লিক করুন।</p>
     </div>
     <div class="pt-6 md:pt-8">
       <div class="md:flex justify-center gap-x-4">
-        <div v-for="(item, i) in productInfo" :key="i"  class="w-full py-2 md:py-0 md:w-[30%]">
-          <div @click="select(item)" :class="selectedProduct ===item ? 'text-[#0381e0] ring-2 ring-[#0381e0]': 'ring-2 ring-[#9fa4a8]'" class="cursor-pointer rounded-md  bg-white px-4 py-2  flex items-center gap-x-4">
-            <div>
-              <img class="h-[90px]" src="/images/product.jpg" alt="image"/>
+        <div v-for="(item, i) in productInfo" :key="i" class="w-full py-2 md:py-0 md:w-[30%]">
+          <div @click="selectedProduct = item"
+               :class="selectedProduct === item ? 'text-[#0381e0] ring-2 ring-[#0381e0]' : 'ring-2 ring-[#9fa4a8]'"
+               class="cursor-pointer rounded-md  bg-white p-2  flex items-center gap-x-4">
+            <div class="bg-[#13619c] text-[50px] font-extrabold text-white px-4 rounded-md italic">
+              {{ 25 + i }}
             </div>
             <div>
-              <p class="text-[20px] font-[600]">{{ item.name }} × ১</p>
-              <p class="text-[20px] font-[600]">৳ {{ item.price }}</p>
-
+              <p class="text-[25px] font-[700]">{{ item.name }}</p>
+              <p class="text-[20px] font-[600]"> <span class="text-red-500 line-through italic pr-4">{{ item.price
+                }}</span> ৳ {{ item.discount_price }}</p>
             </div>
           </div>
         </div>
@@ -24,50 +27,46 @@
     </div>
     <div class="md:flex justify-center pt-8">
       <div class="grid grid-cols-1 md:grid-cols-2 w-full md:w-[60%] gap-x-12">
-        <div>
+        <div class="order-2 md:order-1">
           <div>
             <p class="text-[24px] font-[600]">Billing details</p>
           </div>
           <div class="pt-4">
-            <label for="name" class="text-[16px] font-[600]">আপনার নাম *</label>
-            <input v-model="name" type="text" class="border-2 border-[#0381e0] text-[16px] rounded-md px-2 py-2 flex items-center w-full" placeholder="আপনার নাম"/>
+            <label for="name" class="text-[16px] font-[600]">আপনার নাম <span class="text-red-500">*</span></label>
+            <input v-model="name" type="text"
+                   class="border-2 border-[#0381e0] text-[16px] rounded-md px-2 py-2 flex items-center w-full"
+                   placeholder="আপনার নাম" />
           </div>
           <div class="pt-4">
-            <label for="name" class="text-[16px] font-[600]">মোবাইল নাম্বার *</label>
-            <input v-model="phone" type="number" class="border-2 border-[#0381e0] text-[16px] rounded-md px-2 py-2 flex items-center w-full" placeholder="মোবাইল নাম্বার"/>
+            <label for="name" class="text-[16px] font-[600]">মোবাইল নাম্বার <span class="text-red-500">*</span></label>
+            <input v-model="phone" type="number"
+                   class="border-2 border-[#0381e0] text-[16px] rounded-md px-2 py-2 flex items-center w-full"
+                   placeholder="মোবাইল নাম্বার" />
           </div>
           <div class="pt-4">
-            <label for="name" class="text-[16px] font-[600]">শাখা *</label>
+            <label for="name" class="text-[16px] font-[600]">শাখা <span class="text-red-500">*</span></label>
             <div class="flex gap-x-2 pt-2">
-              <div class="flex items-center gap-x-2">
-                <input v-model="group" type="radio" id="science" name="fav_group" value="science">
-                <label for="science" class="text-[18px]">বিজ্ঞান</label><br>
-              </div>
-              <div class="flex items-center gap-x-2">
-                <input  v-model="group" type="radio" id="commerce" name="fav_group" value="commerce">
-                <label for="commerce" class="text-[18px]">ব্যবসায়</label><br>
-              </div>
-              <div class="flex items-center gap-x-2">
-                <input  v-model="group" type="radio" id="arts" name="fav_group" value="arts">
-                <label for="arts" class="text-[18px]">মানিবিক</label><br>
+              <div v-for="(gru, i) in groups" :key="i">
+                <button @click="group = gru.value" :class="group === gru.value ? 'bg-[#0381E0] text-white' : ''"
+                        class="border-2 border-[#0381E0] px-4 py-2 rounded-md">{{ gru.name }}</button>
               </div>
             </div>
           </div>
         </div>
-        <div class="pt-4 md:pt-0">
+        <div class="pt-4 md:pt-0 order-1 md:order-2">
           <div>
             <p class="text-[24px] font-[600]">Your order</p>
           </div>
           <div class="ring-2 ring-[#0381e0] p-4 mt-6 rounded-md">
             <div class="flex items-center gap-x-6 md:gap-x-12">
               <div class="flex items-center gap-x-4">
-                <img class="h-[50px]" src="/images/product.jpg" alt="image"/>
+                <img class="h-[50px]" src="/images/product.jpg" alt="image" />
                 <p class="text-[18px]">{{ selectedProduct?.name }}</p>
               </div>
               <div class="text-[18px]">৳ {{ selectedProduct?.price }} × {{ quantity }}</div>
             </div>
             <div class="pt-2">
-              <hr/>
+              <hr />
             </div>
             <div class="flex items-center gap-x-12 pt-2">
               <div class="">
@@ -76,7 +75,7 @@
               <div class="text-[18px] font-[600]">৳ {{ subtotal }}</div>
             </div>
             <div class="pt-2">
-              <hr/>
+              <hr />
             </div>
             <div class="flex items-center gap-x-12 pt-2">
               <div class="">
@@ -85,26 +84,38 @@
               <div class="text-[18px] font-[600]">৳ {{ total }}</div>
             </div>
           </div>
-          <div>
-              <button @click="submit" :disabled="buttonDisabled" :class="buttonDisabled ? 'bg-[#9fa4a8] cursor-not-allowed': 'bg-[#0381e0] cursor-pointer'" class=" text-white text-[18px] font-[600] w-full rounded-md px-4 py-2 mt-4">অর্ডার কনফার্ম</button>
+          <div class="mt-4">
+            <p class="flex items-center mt-2 text-[18px]"><icon size="30" color="#0381E0" name="material-symbols:play-arrow-rounded"/> <span class="font-bold ml-2">কুপন আছে ?</span></p>
+            <div class="mt-2">
+              <input v-model="coupon" type="number"
+                     class="border-2 border-[#0381e0] text-[16px] rounded-md px-2 py-2 flex items-center w-full"
+                     placeholder="কুপন নাম্বার" />
+            </div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="flex justify-center">
+      <button @click="submit" :disabled="buttonDisabled"
+              :class="buttonDisabled ? 'bg-[#9fa4a8] cursor-not-allowed' : 'bg-[#0381e0] cursor-pointer'"
+              class=" text-white text-[22px] font-[600] rounded-md px-12 py-2 mt-4">অর্ডার কনফার্ম</button>
     </div>
   </div>
 </template>
 
 <script setup>
 const productInfo = ref([
-  { name:'এইচএসসি ২০২৫', price:970 },
-  { name:'এইচএসসি ২০২৪', price:475 },
+  { name: 'HSC BATCH 2025', value: 'HSC 2025', discount_price: 1000, price: 1500 },
+  { name: 'HSC BATCH 2026', value: 'HSC 2026', discount_price: 1500, price: 2500 },
 ])
+const groups = [{ name: 'Science', value: 'Science' }, { name: 'Business_studies', value: 'Commerce' }, { name: 'Humanities', value: 'Arts' }];
 const selectedProduct = ref(productInfo.value[0]);
 const quantity = ref(1);
 
 const name = ref('');
 const phone = ref(null);
-const group = ref('');
+const group = ref('science');
+const coupon = ref(null)
 
 const subtotal = computed(() => {
   return selectedProduct.value?.price * quantity.value;
@@ -121,16 +132,28 @@ const select = (product) => {
   selectedProduct.value = product;
 };
 
-const submit = () => {
-  const data = {
+const submit = async () => {
+
+  const payload = {
     name: name.value,
-    phone: phone.value,
+    phone: `0${phone.value}`,
     group: group.value,
-    product: selectedProduct.value,
-    quantity: quantity.value,
-    total: total.value
+    level: selectedProduct.value.value,
+    coupon: coupon.value,
+    source: 'etestpaper-fb-campaign'
   }
-  console.log(data);
+
+  const {data, error} = await useFetch('https://api.v2.e-testpaper.com/api/v1/payment/bkash-thirdparty', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+  console.log(data)
+  if(data.value && data.value.GatewayPageURL){
+    window.location.href =data?.value?.GatewayPageURL;
+  }
 };
 
 </script>
