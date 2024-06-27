b5992f6bcb5511ef3a4cc6f5016e120d2aa24397
Void-Brain70 <dasssanik124102@gmail.com>
2024-06-27 21:05:43 +0600

    6th commit

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -8,13 +8,13 @@
 <!--        <p class="font-[600] text-[16px] 2xl:text-[24px]">অফার ফুরিয়ে যাবার আগেই সাবস্ক্রিপশন করুন</p>-->
 <!--      </div>-->
       <div class="pt-4 md:pt-4 text-center">
-        <p class="text-[26px] 2xl:text-[46px] text-[#f32120] font-[600] font-['Hind_Siliguri']">দেশের প্রথম এবং একমাত্র ডিজিটাল টেস্টপেপার</p>
+        <p class="text-[26px] md:text-[56px] text-[#f32120] font-[700] font-['Hind_Siliguri']">দেশের প্রথম এবং একমাত্র ডিজিটাল টেস্টপেপার</p>
       </div>
-      <div class="pt-4 md:pt-6 text-center">
-        <p class="2xl:text-[26px] md:text-justify text-[#000000] font-[600] font-['Hind_Siliguri']">যা এইচএসসি শিক্ষার্থীদের গাইডবই, সাপ্লিমেন্ট এবং টেস্টপেপার এর একমাত্র ডিজিটাল বিকল্প এবং আকর্ষণীয় মূল্যে বছরজুড়ে</p>
-        <p class="2xl:text-[26px] pt-2 md:pt-0 text-[#000000] font-[600] font-['Hind_Siliguri']">এইচএসসি এবং এডমিশন টেস্ট এ কাঙ্ক্ষিত ফলাফল অর্জনে সহায়ক ভূমিকা রাখছে।</p>
+      <div class="pt-4 md:pt-4 text-center">
+        <p class="md:text-[30px] md:text-justify text-[#000000] font-[600] font-['Hind_Siliguri']">যা এইচএসসি শিক্ষার্থীদের <span class="text-blue-500 font-[700]">গাইডবই, সাপ্লিমেন্ট</span> এবং <span class="text-blue-500 font-[700]">টেস্টপেপার</span> এর একমাত্র ডিজিটাল বিকল্প এবং আকর্ষণীয় মূল্যে বছরজুড়ে</p>
+        <p class="2xl:text-[30px] pt-2 md:pt-0 text-[#000000] font-[600] font-['Hind_Siliguri']">বছরজুড়ে <span class="text-blue-500 font-[700]" >এইচএসসি</span> এবং <span class="text-blue-500 font-[700]">এডমিশন টেস্ট</span>  এ কাঙ্ক্ষিত ফলাফল অর্জনে সহায়ক ভূমিকা রাখছে।</p>
       </div>
-      <div class="pt-4 md:pt-6 pb-4 md:pb-8">
+      <div class="pt-4 md:pt-12 pb-4 md:pb-8">
         <iframe class="h-[220px] 2xl:h-[512px] 2xl:w-[1024px] rounded-3xl"
                 src="https://www.youtube.com/embed/pRYcDAdytKY?si=YLgZpJDlLFbjri4K">
         </iframe>
