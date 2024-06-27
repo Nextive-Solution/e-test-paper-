bcb2cb18d771fef28fd31ff7280aacb399f07c08
Void-Brain70 <dasssanik124102@gmail.com>
2024-06-27 22:39:56 +0600

    6th commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
             viewport: 'width=device-width, initial-scale=1',
             title: 'Test Project',
             meta: [
-                // <meta name="description" content="My amazing site">
                 {name: 'description', content: 'Test Project.'}
             ],
         }
