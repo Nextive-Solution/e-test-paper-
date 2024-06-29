c430bc4798a32cc4483c833a0d885ccc16b3d811
parthobepary <pallobbiswas326@gmail.com>
2024-06-29 22:17:16 +0600

    add pixel and gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,20 @@ export default defineNuxtConfig({
             charset: 'utf-16',
             viewport: 'width=device-width, initial-scale=1',
             title: 'E-test Paper',
-        }
+            script: [
+                {
+                    innerHTML: `
+                        (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "mzkt44eclg");`,
+                    type: 'text/javascript',
+                    charset: 'utf-8'
+                },
+                { src: "https://www.facebook.com/tr?id=1527387001207075&ev=PageView&noscript=1", async: true },
+            ]
+        },
     },
     imports: {
         dirs: ['stores'],
@@ -19,6 +32,10 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    plugins: [
+        { src: "~/plugins/gtag.client", mode: "client", ssr: false },
+        { src: "~/plugins/facebook-pixel.client", mode: "client", ssr: false },
+    ],
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', "nuxt-icon", "@nuxt/image"],
     vite: {
         css: {
@@ -31,6 +48,8 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
+            siteName: "e-test paper",
+            gtagId: 'GTM-TSW73ZQ2',
             apiBase: process.env.API_BASE_URL
         }
     }
